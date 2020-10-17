7a7f792fdaac4837b825b46496bd59fd38f29c85
igor209 <igor84734@gmail.com>
2020-10-17 11:55:56 -0300

    conectando a rota post para eutenticação de usuario e senha

--- cliente/js/index.js
+++ cliente/js/index.js
@@ -2,14 +2,23 @@ var main = function(){
     "use strict"
     $(" main button").on("click", function(){
         var input = $("main input").val()
-        alert(input)
-        .get("/"+input, function(response){
+        alert(username)
+        $.get("/"+input, function(response){
             if(response == 404){
                 //escrevo usuario nao encontrado
             }else if(response == err){
                 //tratar o erro
             }else{
-                //peço a senha do usuario
+                
+                $("body main").hide()
+                $("main").append('<div><h1>Nome</h1></div><div><label for="usuario">Entre com a senha</label></div><div><input type="password" name="usuario" class="senha"></div><div><button class="entrar">Entrar</button></div>')
+                $("main .entrar").on("click", function(){
+                    var password = $("main .senha").val()
+                    var username = $("main h1").text()
+                    $.post("/autenticar",{"senha":password, "username":username}, function(response){
+
+                    })
+                })
             }
         })
     })
