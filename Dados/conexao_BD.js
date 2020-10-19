692aaa7c66ae0db7df87e85908bab590a19cde8d
igor209 <igor84734@gmail.com>
2020-10-19 01:04:52 -0300

    atualizando estrutura dos dados

--- Dados/conexao_BD.js
+++ Dados/conexao_BD.js
@@ -4,7 +4,8 @@ mongoose.connect('mongodb://localhost/amazeriffic')
 
 var esquema = mongoose.Schema({
     Usuario: String,
-    Senha: String
+    Senha: String,
+    Tarefas: String
 })
 
 var user = mongoose.model("usuario", esquema)
